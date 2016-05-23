555313b4b2de73ee7fab14375ee96bc2467e8f0d
shermanip <sherman.ip@spc.ox.ac.uk>
2016-05-23 22:36:28 +0100

    The expectation of the sufficient statistics has been added and modified. Experiments on fitting using EM on simulated data has been added. All code involving the truncation method has been fully commented.

--- MATLAB/compoundPoisson/expectation_compoundPoisson.m
+++ MATLAB/compoundPoisson/expectation_compoundPoisson.m
@@ -1,11 +1,64 @@
-function expectation = expectation_compoundPoisson(truncation,x,m,v,psy)
+%FUNCTION: CONDITIONAL EXPECTED SUFFICIENT STATISTICS FOR COMPOUND POISSON
+%PARAMETER:
+    %truncation (positive integer): number of terms in the sum
+    %x (scalar): conditional observable
+    %m (scalar): mean Normal parameter
+    %v (positive scalar): variance Normal parameter
+    %psy (positive scalar): Poisson parameter
+%RETURN:
+    %expectation: E(Y|X=x)
+    %zeta: E(1/Y|X=x)
+    %Z: p_X(x)
+function [expectation,zeta,Z] = expectation_compoundPoisson(truncation,x,m,v,psy)
 
+    %CHECK THE PARAMETERS ARE OF THE CORRECT TYPE
+    checkParameters(truncation,x,m,v,psy);
 
-    y = 1:truncation;
-    Z = partitionFunction(truncation,x,m,v,psy);
+    %if condition that the observed compound poisson varaible is 0...
+    if x==0
+        %expected poisson variable is 0
+        expectation = 0;
+        %expected 1/poisson varaible is not defined
+        zeta = nan;
+        %p_X(x) = infinity
+        Z = nan;
+    %else for positive x...
+    else
+        %define the y's to be sumed
+        y = 1:truncation;
+        %Z = p_X(x) by marginalising the joint distribution
+        Z = sum( exp( -psy + y*log(psy) - gammaln(y+1) - 0.5*log(2*pi) - 0.5*log(y*v) - 0.5*(x-y*m).^2./(v*y) ) );
+        
+        %work out the expectation of Y by truncating the expectation sum
+        expectation = sum( exp( -psy + y*log(psy) - gammaln(y+1) - 0.5*log(2*pi) + 0.5*log(y) - 0.5*log(v) - 0.5*(x-y*m).^2./(v*y) ) ) / Z;
+        
+        %work out the expectation of 1/Y by truncating the expectation sum
+        zeta = sum( exp( -psy + y*log(psy) - gammaln(y+1) - 0.5*log(2*pi) - (3/2)*log(y) - 0.5*log(v) - 0.5*(x-y*m).^2./(v*y) ) ) / Z;
+    end
 
-    expectation = sum( exp( y*log(psy)-gammaln(y)-0.5*log(y)-0.5*(x-m*y).^2./(v*y) ) ) / Z;
-
-
+    %FUNCTION: CHECK THE PARAMETERS ARE OF THE CORRECT TYPE
+    function checkParameters(truncation,x,m,v,psy)
+        %n_samples is a positive integer scalar
+        if ( (~isscalar(truncation)) || (truncation <= 0) || (truncation ~= floor(truncation)) )
+            error('truncation needs to be a positive integer scalar');
+        end
+        %x is a scalar
+        if ~isscalar(x);
+            error('x needs to be a scalar');
+        end
+        %m is a scalar
+        if ~isscalar(m);
+            error('m needs to be a scalar');
+        end
+        %v is a positive scalar
+        if ( (~isscalar(v)) || (v <= 0) );
+            error('v needs to be a positive scalar');
+        end
+        %psy is a positive scalar
+        if ( (~isscalar(psy)) || (psy <= 0) );
+            error('psy needs to be a positive scalar');
+        end
+    end
+    
 end
 
--- MATLAB/compoundPoisson/truncationExp.m
+++ MATLAB/compoundPoisson/truncationExp.m
@@ -1,23 +1,28 @@
 %SCRIPT FOR PLOTTING x VS E[Y|X=x]
 
 %set parameters
-psy = 5;
-m = 1;
-v = 0.01;
+psy = 5; %rate parameter
+m = 3; %mean parameter
+v = 0.1; %variance parameter
 
 %declare array for plots
 x_array = (-10):0.01:10;
 yConditional_array = zeros(1,numel(x_array));
 
+%for each x condition
 for i = 1:numel(x_array)
-    x = x_array(i);
-    yConditional_array(i) = expectation_compoundPoisson(100,x,m,v,psy);
+    x = x_array(i); %get the x
+    %work out the conditional expectation
+    yConditional_array(i) = expectation_compoundPoisson(1000,x,m,v,psy);
 end
 
+%plot the conditional expectation
 figure;
 plot(x_array,yConditional_array);
+%plot the discountinity at x=0
 hold on;
+scatter(0,0,'b','filled');
+hold off;
+%label the axis
 xlabel('Conditional value');
 ylabel('Conditional expectation');
-scatter(0,0,'b','filled');
-scatter(0,yConditional_array(x_array==0),'b');
