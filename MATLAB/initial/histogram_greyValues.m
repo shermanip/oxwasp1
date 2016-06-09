50e1927a5fb4f1220e8ea860f65f553ce47abcd6
shermanip <sherman.ip@spc.ox.ac.uk>
2016-06-09 14:21:31 +0100

    renamed histogram.m

--- MATLAB/initial/histogram_greyValues.m
+++ MATLAB/initial/histogram_greyValues.m
@@ -2,9 +2,12 @@ clc;
 clear all;
 close all;
 
+%load the data
 X = load_stack('/data/tinamou/sip/block_images/orginial');
+%reshape it in a column vector
 X = reshape(X,numel(X),[]);
 
+%plot the histogram
 figure;
 histogram(X,'Normalization','countdensity');
 xlabel('Grey values (arb. unit)');
